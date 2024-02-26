0220f498a39703a92818d66496691812d5af6c16
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-02-26 15:51:57 -0300

    Add files via upload

---
+++ app-i/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { AuthButtonServer } from "../components/auth-button-server";
+
+function Login() {
+    return ( 
+<>
+<h1>Inicia Sesion</h1>
+<AuthButtonServer/>
+</>
+
+
+     );
+}
+
+export default Login;
